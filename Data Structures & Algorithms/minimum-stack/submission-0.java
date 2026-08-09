3319c252b1aff48cfeb8a461ce2df11b02937433
Tanmayee Gosavi <78088064+Tanmayee3012@users.noreply.github.com>
2026-08-09 23:32:50 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,29 @@
+class MinStack {
+    private Stack<int[]> st;
+
+    public MinStack() {
+        st = new Stack<>();
+    }
+    
+    public void push(int val) {
+        if(st.isEmpty()){
+            st.push(new int[] {val, val});
+        } else {
+            int curMin = st.peek()[1];
+            st.push(new int[]{val, Math.min(val, curMin)});
+        }
+        
+    }
+    
+    public void pop() {
+        st.pop();
+    }
+    
+    public int top() {
+        return st.peek()[0];
+    }
+    
+    public int getMin() {
+        return st.peek()[1];
+    }
+}
